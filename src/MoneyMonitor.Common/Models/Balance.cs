59135afb57c829baddae15c1e6ce3cd5a1fd95b3
Stevö <stevehuwjohn@gmail.com>
2021-02-13 01:54:17 +0000

    Initial commit.

---
+++ src/MoneyMonitor.Common/Models/Balance.cs
@@ -0,0 +1,9 @@
+﻿namespace MoneyMonitor.Common.Models
+{
+    public class Balance
+    {
+        public string Currency { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
---
+++ src/MoneyMonitor.Windows/Infrastructure/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace MoneyMonitor.Windows.Infrastructure
+{
+    public static class StartUp
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new Context());
+        }
+    }
+}
