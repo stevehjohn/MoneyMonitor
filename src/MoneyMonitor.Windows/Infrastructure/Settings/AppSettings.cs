488bd823ccca056dbd177ced99560cf2eaf81f62
Stevö <stevehuwjohn@gmail.com>
2021-02-13 13:42:55 +0000

    WIP

---
+++ src/MoneyMonitor.Windows/Infrastructure/Settings/AppSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace MoneyMonitor.Windows.Infrastructure.Settings
+{
+    public class AppSettings
+    {
+        public CoinbaseCredentials CoinbaseCredentials { get; set; }
+
+        public static AppSettings Instance => Lazy.Value;
+
+        private static readonly Lazy<AppSettings> Lazy = new(GetAppSettings);
+
+        private static AppSettings GetAppSettings()
+        {
+            var json = File.ReadAllText("appSettings.json", Encoding.UTF8);
+
+            var settings = JsonSerializer.Deserialize<AppSettings>(json);
+
+            return settings;
+        }
+    }
+}
---
+++ src/MoneyMonitor.Windows/Infrastructure/Settings/CoinbaseCredentials.cs
@@ -0,0 +1,9 @@
+﻿namespace MoneyMonitor.Windows.Infrastructure.Settings
+{
+    public class CoinbaseCredentials
+    {
+        public string ApiKey { get; set; }
+        
+        public string ApiSecret { get; set; }
+    }
+}
