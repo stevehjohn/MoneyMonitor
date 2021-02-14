e76a867f343e3d5388bce2c875ed63da5d69d688
Stevö <stevehuwjohn@gmail.com>
2021-02-14 14:39:54 +0000

    Excel

--- src/MoneyMonitor.Windows/Infrastructure/StartUp.cs
+++ src/MoneyMonitor.Windows/Infrastructure/StartUp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using OfficeOpenXml;
 
 namespace MoneyMonitor.Windows.Infrastructure
 {
@@ -11,6 +12,8 @@ namespace MoneyMonitor.Windows.Infrastructure
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
             Application.Run(new Context());
         }
