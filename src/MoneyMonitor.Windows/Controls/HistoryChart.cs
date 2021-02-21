4a07432bcebd57f4d38e447cb5a06bca14b54d24
Stevö <stevehuwjohn@gmail.com>
2021-02-21 18:31:37 +0000

    ...

--- src/MoneyMonitor.Windows/Controls/HistoryChart.cs
+++ src/MoneyMonitor.Windows/Controls/HistoryChart.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
-using MoneyMonitor.Windows.Infrastructure;
 
 namespace MoneyMonitor.Windows.Controls
 {
--- src/MoneyMonitor.Windows/Infrastructure/Constants.cs
+++ src/MoneyMonitor.Windows/Infrastructure/Constants.cs
@@ -12,8 +12,6 @@
 
         public const int HistoryHeight = 200;
 
-        public const int TextHeight = 15;
-
         public const int BarWidth = 4;
 
         public const int BarSpace = 1;
