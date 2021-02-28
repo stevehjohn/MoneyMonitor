852348cd6d576b4fedd0a8570f1b3c1cd7713ea7
Stevö <stevehuwjohn@gmail.com>
2021-02-28 19:32:57 +0000

    WIP

---
+++ src/MoneyMonitor.Common/Clients/FiatExchangeRateClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoneyMonitor.Common.Clients
+{
+    public class FiatExchangeRateClient
+    {
+        public Dictionary<string, decimal> GetExchangeRates(string baseCurrency)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MoneyMonitor.Common/Services/FiatExchangeRateConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using MoneyMonitor.Common.Clients;
+
+namespace MoneyMonitor.Common.Services
+{
+    public class FiatExchangeRateConverter
+    {
+        private readonly string _baseCurrency;
+
+        private readonly FiatExchangeRateClient _exchangeRateClient;
+
+        private Dictionary<string, decimal> _rates;
+
+        private DateTime _rateAge = DateTime.MinValue;
+
+        public FiatExchangeRateConverter(string baseCurrency)
+        {
+            _baseCurrency = baseCurrency;
+
+            _exchangeRateClient = new FiatExchangeRateClient();
+        }
+
+        public decimal GetValueInBaseCurrency(string currency, decimal value)
+        {
+            RefreshRates();
+
+            return value / _rates[currency];
+        }
+
+        private void RefreshRates()
+        {
+            // TODO: Make rates refresh age config parameter?
+            if (DateTime.UtcNow - _rateAge < TimeSpan.FromHours(12))
+            {
+                return;
+            }
+
+            _rates = _exchangeRateClient.GetExchangeRates(_baseCurrency);
+
+            _rateAge = DateTime.UtcNow;
+        }
+    }
+}
