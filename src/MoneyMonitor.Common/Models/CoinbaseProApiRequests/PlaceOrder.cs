f0f59410074fb85df78a5f402b0f4d4d8135184d
Stevö <stevehuwjohn@gmail.com>
2021-08-31 21:41:08 +0100

    WIP

--- src/MoneyMonitor.Common/Models/CoinbaseProApiRequests/PlaceOrder.cs
+++ src/MoneyMonitor.Common/Models/CoinbaseProApiRequests/PlaceOrder.cs
@@ -7,14 +7,23 @@ namespace MoneyMonitor.Common.Models.CoinbaseProApiRequests
         [JsonPropertyName("cancel_after")]
         public string CancelAfter { get; set; }
 
+        [JsonPropertyName("price")]
+        public string Price { get; set; }
+
         [JsonPropertyName("product_id")]
         public string ProductId { get; set; }
 
         [JsonPropertyName("side")]
         public string Side { get; set; }
 
+        [JsonPropertyName("size")]
+        public string Size { get; set; }
+
         [JsonPropertyName("stop")]
         public string Stop { get; set; }
+
+        [JsonPropertyName("stop_price")]
+        public string StopPrice { get; set; }
 
         [JsonPropertyName("time_in_force")]
         public string TimeInForce { get; set; }
